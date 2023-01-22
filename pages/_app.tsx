61868f6a46fde2b3c13b9ce0d71c581288b6847b
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-22 23:20:54 +0000

    add changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import GitHubButton from 'react-github-button';
 import { AlertCircle, Box, GitHub } from 'react-feather';
 import '../styles/globals.css';
-import Support from 'components/support';
 
 function MyApp({ Component, pageProps }) {
 	return (
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps }) {
 						tiddlywiki-storage
 					</a>
 				</Link>
-
 				<nav>
 					<a href="https://github.com/Devzstudio/Web3UIKit/issues/new" target="_blank" rel="noreferrer">
 						<AlertCircle /> Report bugs
@@ -24,26 +22,15 @@ function MyApp({ Component, pageProps }) {
 					</div>
 				</nav>
 			</header>
-
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
 				href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500&display=swap"
 				rel="stylesheet"
 			></link>
-
 			<main className="container">
-				<Support />
-
 				<Component {...pageProps} />
 			</main>
-
-			<footer>
-				Made with <img width="14" src="https://twemoji.maxcdn.com/2/72x72/1f496.png" alt="heart"></img> by
-				<a href="https://devzstudio.com" target="_blank" rel="noreferrer">
-					Devzstudio
-				</a>
-			</footer>
 		</>
 	);
 }

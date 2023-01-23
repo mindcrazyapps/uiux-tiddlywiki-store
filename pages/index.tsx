f2f0a332740934c668a67ed91a43e3e43b00593e
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 07:33:06 +0000

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,13 @@ export default function Home() {
           content="tiddlywiki-storage by mindcrazyapps"
         />
         <link rel="icon" href="/static/favicon.ico"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon.ico">
+	      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon.ico">
+	      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon.ico">
+	      <link rel="manifest" href="/site.webmanifest">
+	      <link rel="mask-icon" href="/static/favicon.ico" color="#5bbad5">
+	      <meta name="msapplication-TileColor" content="#da532c">
+	      <meta name="theme-color" content="#ffffff">
       </Head>
     </div>
   )

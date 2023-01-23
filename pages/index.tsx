a6baa13bec0f46015e45e25161e6cfbbf34e9b9d
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 07:36:22 +0000

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
     <div>
       <Head>
         <title>tiddlywiki-storage</title>
@@ -10,14 +11,15 @@ export default function Home() {
           content="tiddlywiki-storage by mindcrazyapps"
         />
         <link rel="icon" href="/static/favicon.ico"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon.ico">
-	      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon.ico">
-	      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon.ico">
-	      <link rel="manifest" href="/site.webmanifest">
-	      <link rel="mask-icon" href="/static/favicon.ico" color="#5bbad5">
+        <link rel="apple-touch-icon" sizes="180x180"  href="/static/favicon.ico"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon.ico"/>
+        <link rel="icon" type="image/png" sizes="16x16"  href="/static/favicon.ico"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/static/favicon.ico" color="#5bbad5"/>
 	      <meta name="msapplication-TileColor" content="#da532c">
-	      <meta name="theme-color" content="#ffffff">
+	      <meta name="theme-color" content="#ffffff"></meta>
       </Head>
     </div>
+    </>
   )
 }

95e5d901274c62cb4f95d8bce1a73aea56748638
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 09:11:35 +0000

    add

--- components/preview_component/index.tsx
+++ components/preview_component/index.tsx
@@ -5,11 +5,29 @@ import { Code, Eye, Image } from 'react-feather'
 import { useRouter } from "next/router";
 
 const PreviewComponent = ({ Component, preview, name, source }) => {
-  const [tab, setTab] = useState(1)
+  return (
+    <>
+    <Image />
+    <Code />
+    <Eye />
+    <img className="preview_image" src={preview} width="100%" height="100%" />
+    <Component />
+    <SourceCode source={source} />
+    </>
+  )
+}
+
+export default PreviewComponent
+
+/*
+
   const router = useRouter();
+
+  const [tab, setTab] = useState(1)
+
   const handleClick = (e, path) => {
     if (path === "#/preview") {
-      return <img className="preview_image" src={preview} width="100%" height="100%"/>
+      return <img className="preview_image" src={preview} width="100%" height="100%" />
     }
     if (path === "#/demo") {
       return <Component />
@@ -18,36 +36,20 @@ const PreviewComponent = ({ Component, preview, name, source }) => {
       return <SourceCode source={source} />
     }
   };
-  return (
-    <div className="tab_container">
-      <div className="tab-header">
-        <div className="tabs">
-          <Link href="/">
-            <Image />
-            <a onClick={(e) => handleClick(e, "#/preview")} hx-post="/clicked" hx-swap="outerHTML">Preview</a>
-          </Link>
-          <Link href="/">
-            <Eye />
-            <a onClick={(e) => handleClick(e, "#/demo")} hx-post="/clicked" hx-swap="outerHTML">Demo</a>
-          </Link>
-          <Link href="/">
-            <Code />
-            <a onClick={(e) => handleClick(e, "#/source-code")} hx-post="/clicked" hx-swap="outerHTML">Source-code</a>
-          </Link>
-        </div>
-      </div>
-      {tab === 1 && (
-        <img
-          className="preview_image"
-          src={preview}
-          width="100%"
-          height="100%"
-        />
-      )}
-      {tab === 2 && <Component />}
-      {tab === 3 && <SourceCode source={source} />}
-    </div>
-  )
-}
 
-export default PreviewComponent
+  <>
+    <Component />
+        <Link href="/">
+          <Image />
+          <a onClick={(e) => handleClick(e, "#/preview")} hx-post="/clicked" hx-swap="outerHTML">Preview</a>
+        </Link>
+        <Link href="/">
+          <Eye />
+          <a onClick={(e) => handleClick(e, "#/demo")} hx-post="/clicked" hx-swap="outerHTML">Demo</a>
+        </Link>
+        <Link href="/">
+          <Code />
+          <a onClick={(e) => handleClick(e, "#/source-code")} hx-post="/clicked" hx-swap="outerHTML">Source-code</a>
+        </Link>
+    </>
+*/

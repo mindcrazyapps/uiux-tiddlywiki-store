cab80e970611529d24479638d04614eb405d6978
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 08:43:01 +0000

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,14 @@ export default function Home() {
           name="description"
           content="tiddlywiki-storage by mindcrazyapps"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/static/favicon.ico"/>
         <link rel="apple-touch-icon" sizes="180x180"  href="/static/favicon.ico"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon.ico"/>
         <link rel="icon" type="image/png" sizes="16x16"  href="/static/favicon.ico"/>
         <link rel="manifest" href="/site.webmanifest"/>
         <link rel="mask-icon" href="/static/favicon.ico" color="#5bbad5"/>
-	      <meta name="theme-color" content="#ffffff"></meta>
+	      <meta name="theme-color" content="#ffffff"/>
         <script src="https://unpkg.com/htmx.org@1.8.5"></script>
       </Head>
     </div>

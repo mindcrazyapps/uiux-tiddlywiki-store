e134cdf681b36cb4d77cffd67723f4433715b508
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 07:18:57 +0000

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           name="description"
           content="tiddlywiki-storage by mindcrazyapps"
         />
-        <link rel="icon" href="/static/favicon.ico"/>
+        <link rel="icon" href="./static/favicon.ico"/>
       </Head>
     </div>
   )

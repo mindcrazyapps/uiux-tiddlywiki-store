6e6c8533607946e20eca50b0b2c7814b7f3a8651
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-23 08:47:12 +0000

    changes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/components/choose_storage',
-      },
-    ]
-  },
-  images: {
-    domains: ['tiddlywiki.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tiddlywiki.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };

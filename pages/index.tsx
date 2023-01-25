cf9c6c2310e5b93f2e5d3ff339b9c2667329f0ab
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-01-25 00:38:28 +0000

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>tiddlywiki-store</title>
         <meta
           name="description"
-          content="tiddlywiki-store by mindcrazyapps"
+          content="tiddlywiki-store by nextexperience"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/static/favicon.ico"/>
@@ -18,8 +18,6 @@ export default function Home() {
         {/* <link rel="manifest" href="/site.webmanifest"/> */}
         <link rel="mask-icon" href="/static/favicon.ico" color="#5bbad5"/>
 	      <meta name="theme-color" content="#ffffff"/>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/json2html/2.1.0/json2html.min.js" async defer/>
-        <script  type="text/javascript" src="https://unpkg.com/htmx.org@1.8.5/dist/htmx.min.js" async defer/>
       </Head>
     </div>
     </>
